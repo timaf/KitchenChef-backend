277c6c881028f8c90fc78f15c2c7fcde5a2f1374
Waelazar <waelazar@gmail.com>
2018-05-08 19:31:12 +0200

    create add cooking event

--- src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
+++ src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
@@ -1,9 +1,30 @@
 package at.refugeesCode.kitchenchefbackend.endpoint;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import at.refugeesCode.kitchenchefbackend.persistence.model.Meal;
+import at.refugeesCode.kitchenchefbackend.persistence.repository.MealRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cook")
 public class CookEndpoint {
+
+    private MealRepository mealRepository;
+
+    public CookEndpoint(MealRepository mealRepository) {
+        this.mealRepository = mealRepository;
+    }
+
+    @PostMapping
+    Meal createMael(@RequestBody Meal meal){
+        mealRepository.save(meal);
+        return meal;
+    }
+
+    @GetMapping
+    List<Meal> getAllMeals(){
+        return mealRepository.findAll();
+    }
+
 }
