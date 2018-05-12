6eb46d26cf2f3eef9d4fb969daa62983f0339951
Waelazar <waelazar@gmail.com>
2018-05-12 23:28:11 +0200

    15: As a cook I want to write how many hours I expect to be cooking (meal starts -> cooking starts)

--- src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
+++ src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
@@ -4,6 +4,10 @@ import at.refugeesCode.kitchenchefbackend.persistence.model.Meal;
 import at.refugeesCode.kitchenchefbackend.persistence.repository.MealRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @RestController
@@ -18,6 +22,18 @@ public class CookEndpoint {
 
     @PostMapping
     Meal createMael(@RequestBody Meal meal){
+
+        LocalDate dateOfEvent = LocalDate.of(meal.getYear(), meal.getMonth(), meal.getDay());
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd LLLL yyyy");
+
+        String format = dateFormat.format(dateOfEvent);
+        meal.setDateTime(format);
+
+        LocalTime startTime = meal.getStartTime();
+        LocalTime cookTime = meal.getCookTime();
+        long between = ChronoUnit.MINUTES.between(startTime, cookTime);
+        meal.setPreparationTime(between);
+
         mealRepository.save(meal);
         return meal;
     }
