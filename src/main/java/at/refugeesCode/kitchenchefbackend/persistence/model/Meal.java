7eb023492f8ba47045b3cdd0aff2cdcd899f059c
Waelazar <waelazar@gmail.com>
2018-05-12 23:31:01 +0200

    add more attr

--- src/main/java/at/refugeesCode/kitchenchefbackend/persistence/model/Meal.java
+++ src/main/java/at/refugeesCode/kitchenchefbackend/persistence/model/Meal.java
@@ -3,8 +3,7 @@ package at.refugeesCode.kitchenchefbackend.persistence.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Document
 public class Meal {
@@ -15,17 +14,29 @@ public class Meal {
     private String mealName;
     private String mealDescription;
     private String ingredients;
-    private Duration preparationTime;
-    private LocalDateTime dateTime;
+
+    private int year;
+    private int month;
+    private int day;
+
+    private LocalTime startTime;
+    private LocalTime cookTime;
+    private Long preparationTime;
+    private String dateTime;
 
     public Meal() {
     }
 
-    public Meal(String username, String mealName, String mealDescription, String ingredients, Duration preparationTime, LocalDateTime dateTime) {
+    public Meal(String username, String mealName, String mealDescription, String ingredients, int year, int month, int day, LocalTime startTime, LocalTime cookTime, Long preparationTime, String dateTime) {
         this.username = username;
         this.mealName = mealName;
         this.mealDescription = mealDescription;
         this.ingredients = ingredients;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.startTime = startTime;
+        this.cookTime = cookTime;
         this.preparationTime = preparationTime;
         this.dateTime = dateTime;
     }
@@ -70,32 +81,77 @@ public class Meal {
         this.ingredients = ingredients;
     }
 
-    public Duration getPreparationTime() {
+    public Long getPreparationTime() {
         return preparationTime;
     }
 
-    public void setPreparationTime(Duration preparationTime) {
+    public void setPreparationTime(Long preparationTime) {
         this.preparationTime = preparationTime;
     }
 
-    public LocalDateTime getDateTime() {
+    public String getDateTime() {
         return dateTime;
     }
 
-    public void setDateTime(LocalDateTime dateTime) {
+    public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(LocalTime cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
     }
 
     @Override
     public String toString() {
-        return "Food{" +
+        return "Meal{" +
                 "id='" + id + '\'' +
                 ", username='" + username + '\'' +
                 ", mealName='" + mealName + '\'' +
                 ", mealDescription='" + mealDescription + '\'' +
                 ", ingredients='" + ingredients + '\'' +
+                ", year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                ", startTime=" + startTime +
+                ", cookTime=" + cookTime +
                 ", preparationTime=" + preparationTime +
-                ", dateTime=" + dateTime +
+                ", dateTime='" + dateTime + '\'' +
                 '}';
     }
 }
