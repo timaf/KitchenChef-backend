5fb7c67c9642980e3aaafdb914fb6de36ec6d629
Waelazar <waelazar@gmail.com>
2018-05-13 21:27:22 +0200

    #20: create assistant as helper or ...

--- src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
+++ src/main/java/at/refugeesCode/kitchenchefbackend/endpoint/CookEndpoint.java
@@ -1,10 +1,13 @@
 package at.refugeesCode.kitchenchefbackend.endpoint;
 
+import at.refugeesCode.kitchenchefbackend.persistence.model.AppUser;
+import at.refugeesCode.kitchenchefbackend.persistence.model.Assistants;
 import at.refugeesCode.kitchenchefbackend.persistence.model.Meal;
+import at.refugeesCode.kitchenchefbackend.persistence.repository.AssistantsRepository;
 import at.refugeesCode.kitchenchefbackend.persistence.repository.MealRepository;
+import at.refugeesCode.kitchenchefbackend.persistence.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -13,17 +16,21 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/cook")
+@RequestMapping("/")
 public class CookEndpoint {
 
     private MealRepository mealRepository;
+    private UserRepository userRepository;
+    private AssistantsRepository assistantsRepository;
 
-    public CookEndpoint(MealRepository mealRepository) {
+    public CookEndpoint(MealRepository mealRepository, UserRepository userRepository, AssistantsRepository assistantsRepository) {
         this.mealRepository = mealRepository;
+        this.userRepository = userRepository;
+        this.assistantsRepository = assistantsRepository;
     }
 
     @PostMapping
-    Meal createMael(@RequestBody Meal meal){
+    Meal createMael(@RequestBody Meal meal, @RequestParam Assistants assistant){
 
         LocalDate dateOfEvent = LocalDate.of(meal.getYear(), meal.getMonth(), meal.getDay());
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd LLLL yyyy");
@@ -37,12 +44,23 @@ public class CookEndpoint {
         meal.setPreparationTime(between);
 
         mealRepository.save(meal);
+        assistantsRepository.save(assistant);
         return meal;
     }
 
-    @GetMapping
+    @GetMapping("/meals")
     List<Meal> getAllMeals(){
         return mealRepository.findAll();
+    }
+
+    @GetMapping("/users")
+    List<AppUser> getAllusers(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/newuser")
+    AppUser addUser(@RequestBody AppUser newUser){
+        return userRepository.save(newUser);
     }
 
     @PutMapping("/delete/{id}")
@@ -71,5 +89,21 @@ public class CookEndpoint {
         }
         return mealEdit.get();
 
+    }
+
+    @PutMapping("/assistant/{mealId}/{userId}/{assistant}")
+    Meal assistants(@PathVariable("assistant") String assistantId, @PathVariable("userId") String userId,
+                    @PathVariable("mealId") String mealId, @RequestParam String assistant){
+        Optional<AppUser> user_Id = userRepository.findById(userId);
+        Optional<Meal> meal_Id = mealRepository.findById(mealId);
+        if(user_Id.isPresent()){
+            String Name = user_Id.get().getUsername();
+            if(meal_Id.isPresent()){
+                Optional<Assistants> assistant_id = assistantsRepository.findById(assistantId);
+                assistant_id.get().setAssistant(assistant);
+            }
+            return meal_Id.get();
+        }
+        return meal_Id.get();
     }
 }
