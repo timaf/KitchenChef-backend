9b76cb505fbed6afb3ec6bd390295e2efc14deae
alirezaasghari <alireza.asghari87@gmail.com>
2018-05-26 19:01:55 +0200

    Working on security

--- src/main/java/at/refugeesCode/kitchenchefbackend/persistence/repository/UserRepository.java
+++ src/main/java/at/refugeesCode/kitchenchefbackend/persistence/repository/UserRepository.java
@@ -3,5 +3,8 @@ package at.refugeesCode.kitchenchefbackend.persistence.repository;
 import at.refugeesCode.kitchenchefbackend.persistence.model.AppUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<AppUser, String> {
+    Optional<AppUser> findOneByUsername(String username);
 }
